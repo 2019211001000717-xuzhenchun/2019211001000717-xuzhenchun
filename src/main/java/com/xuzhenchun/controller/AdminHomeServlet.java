2a53efd70306a655ad6e92a6ac46d4fc513373c6
xuzhenchun <974284350@qq.com>
2021-05-10 21:21:35 +0800

    HW-Week 9 Commit

---
+++ src/main/java/com/xuzhenchun/controller/AdminHomeServlet.java
@@ -0,0 +1,21 @@
+package com.xuzhenchun.controller;
+
+import com.xuzhenchun.model.User;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "AdminHomeServlet", value = "/AdminHomeServlet")
+public class AdminHomeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
